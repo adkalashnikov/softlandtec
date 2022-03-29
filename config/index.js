97704cee7920496ed8988bef5ecc573d1ccff0a3
Denis K <adkalashnikov@gmail.com>
2022-03-29 18:20:09 +0300

    сдача проекта

--- config/index.js
+++ config/index.js
@@ -4,5 +4,6 @@ module.exports = {
     entry: {
         'base': path.resolve(__dirname, '../src/main.js'),
         'index': path.resolve(__dirname, '../src/entryPagePoints/home.js'),
+        'form-submited': path.resolve(__dirname, '../src/entryPagePoints/form-submited.js'),
     },
 };
