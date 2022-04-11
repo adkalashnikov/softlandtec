d0cb722839e556e683127b437bd0d77c61862b66
Denis K <adkalashnikov@gmail.com>
2022-04-11 12:44:51 +0300

    updated turnkey order form

--- src/assets/js/components/hire-forms.js
+++ src/assets/js/components/hire-forms.js
@@ -5,44 +5,55 @@ document.addEventListener('DOMContentLoaded', () => {
     const btnAddSpecialist = document.querySelector('.js-show-s-form'),
         btnAddTurnkey = document.querySelector('.js-show-t-form'),
         btnStep2Hire = document.querySelector('.js-hire-st2-btn'),
-        body = document.querySelector('body'),
         formStep2 = document.querySelector('.form-hire-st2'),
+        formStep2Turnkey = document.querySelector('.js-form-st2-turnkey'),
         formStep3 = document.querySelector('.form-hire-st3'),
         formStep2Alert = document.querySelector('.js-form-st2-alert'),
+        formStep2TurnkeyAlert = document.querySelector('.js-st2-turnkey-alert'),
         tagsStep2 = document.querySelector('.js-st-2-tags'),
         step1Section = document.querySelector('.st-1'),
-        step2Section = document.querySelector('.st-2'),
+        step2Section = document.querySelector('.js-st2-specialist'),
+        step2TurnkeySection = document.querySelector('.js-st2-turnkey'),
         step3Section = document.querySelector('.st-3'),
         step3TextArea = document.querySelector('.js-st-3-textarea'),
         step3TagsWrapper = document.querySelector('.js-st-3-tags'),
         input1 = document.querySelector('.input-tag-p1'),
         input2 = document.querySelector('.input-tag-p2'),
-        input3 = document.querySelector('.input-tag-p3');
+        input3 = document.querySelector('.input-tag-p3'),
+        inputPName = document.querySelector('.js-input-p-name'),
+        inputPDesc = document.querySelector('.js-input-p-desc'),
+        inputPDescCounter = document.querySelector('.js-input-p-desc-count');
 
-    let isFieldsEmpty = true,
-        isSpecialistsAdded = false,
+    let isSpecialistsAdded = false,
+        isTurnkeyFormFilled = false,
         orderDetails = '';
 
-    function checkIsFieldEmpty(input) {
-        return input.value.length > 0 && input.value !== '' && input.value !== '0';
+    function checkIsFieldEmpty(input, valueLength = 0) {
+        return input.value.length > valueLength && input.value !== '' && input.value !== '0';
     }
 
     function validateStep2Field(input) {
         if(!checkIsFieldEmpty(input)) {
             input.classList.add('error');
-            isFieldsEmpty = true;
+            return false;
         } else {
             input.classList.remove('error');
-            isFieldsEmpty = false;
+            return true;
+        }
+    }
+
+    function validateStep2TurnkeyField(input) {
+        if(!checkIsFieldEmpty(input, 2)) {
+            input.classList.add('error');
+            return false;
+        } else {
+            input.classList.remove('error');
+            return true;
         }
     }
 
     function addSpecialistTag() {
-        validateStep2Field(input1);
-        validateStep2Field(input2);
-        validateStep2Field(input3);
-
-        if(!isFieldsEmpty) {
+        if(validateStep2Field(input1) && validateStep2Field(input2) && validateStep2Field(input3)) {
             tagsStep2.innerHTML += `<div class="f-tag" data-technology="${input1.value}"  data-snum="${input2.value}"  data-enum="${input3.value}">
                 <span class="f-tag-text">${input1.value}, ${input3.value} years</span>
                 <span class="f-tag-delete"
@@ -57,6 +68,101 @@ document.addEventListener('DOMContentLoaded', () => {
         } else {
             isSpecialistsAdded = false;
         }
+    }
+
+    function deleteTagsHandler() {
+        document.addEventListener('click', (e) => {
+            if(e.target.classList.contains('f-tag-delete')) {
+                e.target.closest('.f-tag').remove();
+            }
+        });
+    }
+
+    function closeModalByESC() {
+        document.addEventListener('keydown', (e) => {
+            if(e.keyCode === 27) {
+                isSpecialistsAdded = false;
+                isTurnkeyFormFilled = false;
+                step1Section.classList.remove('hidden');
+                step2Section.classList.add('hidden');
+                step2TurnkeySection.classList.add('hidden');
+                step3Section.classList.add('hidden');
+                formStep2.reset();
+                tagsStep2.innerHTML = '';
+                formStep2Alert.classList.add('hidden');
+                formStep2TurnkeyAlert.classList.add('hidden');
+                formStep2Turnkey.reset();
+                formStep3.reset();
+                step3TagsWrapper.innerHTML = '';
+                orderDetails = '';
+                step2ClearFields();
+                step2TurnkeyClearFields();
+                inputPDescCounter.innerHTML = 'Characters count: 0/1250';
+            }
+        });
+    }
+
+    function step2TurnkeyDescriptionHandler() {
+        inputPDesc.addEventListener('keyup', () => {
+            const textArea = inputPDesc.value.length;
+            inputPDescCounter.innerHTML = `Characters count: ${textArea}/1250`;
+        });
+        inputPDesc.addEventListener('keydown', () => {
+            const textArea = inputPDesc.value.length;
+            inputPDescCounter.innerHTML = `Characters count: ${textArea}/1250`;
+        });
+    }
+
+    function initSpecialistOrder() {
+        btnAddSpecialist.addEventListener('click', () => {
+            step1Section.classList.add('hidden');
+            step2Section.classList.remove('hidden');
+        });
+    }
+
+    function initTurnkeyOrder() {
+        btnAddTurnkey.addEventListener('click', () => {
+            step1Section.classList.add('hidden');
+            step2TurnkeySection.classList.remove('hidden');
+        });
+    }
+
+    function step2ClearFields() {
+        input1.classList.remove('error');
+        input2.classList.remove('error');
+        input3.classList.remove('error');
+    }
+
+    function step2TurnkeyClearFields() {
+        inputPName.classList.remove('error');
+        inputPDesc.classList.remove('error');
+    }
+
+    function step2SpecialistAddHandler() {
+        btnStep2Hire.addEventListener('click', (e) => {
+            e.preventDefault();
+            addSpecialistTag();
+        });
+    }
+
+    function step2SpecialistFormHandler() {
+        formStep2.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            if(!isSpecialistsAdded) {
+                addSpecialistTag();
+            }
+
+            if(isSpecialistsAdded) {
+                formStep2Alert.classList.add('hidden');
+                step2Section.classList.add('hidden');
+                step3AddTags();
+                step3TextArea.value  = `Request \n\nType: ${formStep2.dataset.orderType} \n\nDetails:${orderDetails}`;
+                step3Section.classList.remove('hidden');
+            } else {
+                formStep2Alert.classList.remove('hidden');
+            }
+        });
     }
 
     function step3AddTags() {
@@ -74,69 +180,38 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
-    btnAddSpecialist.addEventListener('click', () => {
-        step1Section.classList.add('hidden');
-        step2Section.classList.remove('hidden');
-        formStep2.dataset.orderType = 'Specialist';
-    });
-
-    btnAddTurnkey.addEventListener('click', () => {
-        step1Section.classList.add('hidden');
-        step2Section.classList.remove('hidden');
-        body.classList.add('modal-dark');
-        step2Section.classList.add('dark');
-        step3Section.classList.add('dark');
-        formStep2.dataset.orderType = 'Turnkey Order';
-    });
-
-    btnStep2Hire.addEventListener('click', (e) => {
-        e.preventDefault();
-        addSpecialistTag();
-    });
-
-    document.addEventListener('click', (e) => {
-        if(e.target.classList.contains('f-tag-delete')) {
-            e.target.closest('.f-tag').remove();
-        }
-    });
-
-    formStep2.addEventListener('submit', (e) => {
-        e.preventDefault();
-
-        if(!isSpecialistsAdded) {
-            addSpecialistTag();
-        }
-
-        if(isSpecialistsAdded) {
+    function validateStep2TurnkeyForm() {
+        if(validateStep2TurnkeyField(inputPName) && validateStep2TurnkeyField(inputPDesc)) {
+            isTurnkeyFormFilled = true;
             formStep2Alert.classList.add('hidden');
-            step2Section.classList.add('hidden');
-            step3AddTags();
-            step3TextArea.value  = `Request \n\nType: ${formStep2.dataset.orderType} \n\nDetails:${orderDetails}`;
-            step3Section.classList.remove('hidden');
         } else {
-            formStep2Alert.classList.remove('hidden');
+            isTurnkeyFormFilled = false;
+            formStep2TurnkeyAlert.classList.remove('hidden');
         }
-    });
+    }
 
-    document.addEventListener('keydown', (e) => {
-        if(e.keyCode === 27) {
-            isFieldsEmpty = true;
-            isSpecialistsAdded = false;
-            step1Section.classList.remove('hidden');
-            step2Section.classList.add('hidden');
-            step2Section.classList.remove('dark');
-            step3Section.classList.add('hidden');
-            step3Section.classList.remove('dark');
-            body.classList.remove('modal-dark');
-            formStep2.dataset.orderType = '';
-            formStep2.reset();
-            tagsStep2.innerHTML = '';
-            formStep2Alert.classList.add('hidden');
-            formStep3.reset();
-            step3TagsWrapper.innerHTML = '';
-            orderDetails = '';
-        }
-    });
+    function step2TurnkeyFormHandler() {
+        formStep2Turnkey.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            validateStep2TurnkeyForm();
+
+            if(isTurnkeyFormFilled) {
+                step2TurnkeySection.classList.add('hidden');
+                step3TextArea.value  = `Request \n\nType: ${formStep2Turnkey.dataset.orderType} \nProject: ${inputPName.value} \n\nDescription:\n${inputPDesc.value}`;
+                step3Section.classList.remove('hidden');
+            }
+        });
+    }
+
+    initSpecialistOrder();
+    initTurnkeyOrder();
+    step2SpecialistAddHandler();
+    step2SpecialistFormHandler();
+    step2TurnkeyFormHandler();
+    step2TurnkeyDescriptionHandler();
+    deleteTagsHandler();
+    closeModalByESC();
     // /hire forms
 
     // tags tooltips
