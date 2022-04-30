e529ce4d570be1d9a373ce0ed0bf8e4dbf4dbb15
Denis K <adkalashnikov@gmail.com>
2022-04-30 08:28:08 +0300

    hero section - deleted video background

--- src/entryPagePoints/home.js
+++ src/entryPagePoints/home.js
@@ -4,33 +4,6 @@ import scrollToElem from 'js/components/scrollToElem';
 import modal from 'js/components/modal';
 
 document.addEventListener('DOMContentLoaded', () => {
-    // lazy load video background
-    let lazyVideos = [].slice.call(document.querySelectorAll("video.js-video-bg"));
-
-    if ("IntersectionObserver" in window) {
-        let lazyVideoObserver = new IntersectionObserver(function(entries, observer) {
-            entries.forEach(function(video) {
-                if (video.isIntersecting) {
-                    for (let source in video.target.children) {
-                        let videoSource = video.target.children[source];
-                        if (typeof videoSource.tagName === "string" && videoSource.tagName === "SOURCE") {
-                            videoSource.src = videoSource.dataset.src;
-                        }
-                    }
-
-                    video.target.load();
-                    video.target.classList.remove("lazy");
-                    lazyVideoObserver.unobserve(video.target);
-                }
-            });
-        });
-
-        lazyVideos.forEach(function(lazyVideo) {
-            lazyVideoObserver.observe(lazyVideo);
-        });
-    }
-    // /lazy load video background
-
     // show more text in about us cards
     const moreContentTriggers = [].slice.call(document.querySelectorAll('.js-show-more-text'));
 
